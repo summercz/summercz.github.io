0d67c725a58bef9b0af2d02d17434ee0a6bdfa33
summercz <summerzhou@byui.edu>
2021-01-31 03:19:00 -0800

    L4

---
+++ lesson4/script/javascript.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    // var ts = new Date();
+    // console.log(ts.toDateString());
+    document.getElementById("currentdate").innerHTML = new Date().toDateString();
+    // document.getElementById("lastupdatetime").innerHTML = new Date(document.lastModified).toLocaleString();
+}
+  
+myFunction()
+
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classlist.toggle("hide");
+}
