db6158f1d4b09f498e380ee6955ec25628fa59fe
summercz <summerzhou@byui.edu>
2021-02-20 20:34:52 -0800

    lesson7

--- lesson7/script/javascript.js
+++ lesson7/script/javascript.js
@@ -28,4 +28,18 @@ if ("IntersectionObserver" in window) {
     imgObserver.observe(img);
   });
 } else { // just load all images if not supported
+
+  function myFunction() {
+    // var ts = new Date();
+    // console.log(ts.toDateString());
+    document.getElementById("currentdate").innerHTML = new Date().toDateString();
+    // document.getElementById("lastupdatetime").innerHTML = new Date(document.lastModified).toLocaleString();
+}
+  
+myFunction()
+
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
 }
