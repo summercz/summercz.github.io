53cb7decf6853dfe53d10d985e76db4756235061
summercz <summerzhou@byui.edu>
2021-03-20 03:52:53 -0700

    move everything to lesson 11

---
+++ lesson11/script/javascript.js
@@ -0,0 +1,53 @@
+//get all imgs with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {image.removeAttribute("data-src");};
+};
+
+//first check to see if Intersection Observer is supported
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  //loops through each img and check sttus and load if necessary
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else { // just load all images if not supported
+  
+}
+
+function myFunction() {
+    // var ts = new Date();
+    // console.log(ts.toDateString());
+    document.getElementById("currentdate").innerHTML = new Date().toDateString();
+    // document.getElementById("lastupdatetime").innerHTML = new Date(document.lastModified).toLocaleString();
+}
+  
+myFunction()
+
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+  }
+
+var d = new Date ();
+var n = d.getDay ();
+
+if(n !=5) {
+    document.getElementById("announcement").style.display = "none";
+}
---
+++ lesson11/script/weatherapi.js
@@ -0,0 +1,40 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=ce4a7567162ee2c718a59cbec4e90ea5&units=imperial';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const weather_description = jsObject.weather[0].description;
+    const current_temp = parseFloat(jsObject.main.temp);
+    const high_temp = parseFloat(jsObject.main.temp_max);
+    const humidity = parseFloat(jsObject.main.humidity);
+    const windspeed = parseFloat(jsObject.wind.speed);
+    wind_chill = 35.74 + 0.6215 * current_temp - 35.75 * windspeed ** 0.16 + 0.4275 * current_temp * windspeed ** 0.16;
+    
+    document.getElementById("weather_description").innerHTML = weather_description;
+    document.getElementById("current_temp").innerHTML = current_temp.toFixed(0) + " °F";
+    document.getElementById("high_temp").innerHTML = high_temp.toFixed(0) + " °F";
+    document.getElementById("windchill").innerHTML = wind_chill.toFixed(0) + " °F";
+    document.getElementById("humidity").innerHTML = humidity.toFixed(0) + "%";
+    document.getElementById("windspeed").innerHTML = windspeed.toFixed(0) + "MPH";
+
+});
+
+const apiURL2 = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ce4a7567162ee2c718a59cbec4e90ea5&units=imperial';
+fetch(apiURL2)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        var forecast_list = jsObject.list.filter(forecast => forecast.dt_txt.includes('18:00:00'));
+        console.table(forecast_list);
+
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+        for (let i = 0; i < forecast_list.length; i++) {
+            const date = new Date(forecast_list[i].dt_txt);
+            const imgSrc = `https://openweathermap.org/img/w/${forecast_list[i].weather[0].icon}.png`;
+
+
+            document.getElementById(`day${i}`).textContent = weekdays[date.getDay()];
+            document.getElementById(`temp${i}`).textContent = forecast_list[i].main.temp.toFixed(0) + " °F";
+            document.getElementById(`icon${i}`).setAttribute('src', imgSrc);
+        }
+});
